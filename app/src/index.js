9a4dcfb9e5d708b9768bf463ec4b475aa3015dc3
RahimliZakir <zakirer@code.edu.az>
2023-11-23 04:11:24 +0100

    Apollo Client Integrated

--- app/src/index.js
+++ app/src/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const apolloClient = new ApolloClient({
+  uri: process.env.GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
